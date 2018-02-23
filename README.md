940121565d4eff6ee76c6c45e92f7c3b9a4a5172
victorvianna <victor.vianna10@gmail.com>
2018-02-23 11:15:30 +0100

    readme edition

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 
 
 Application for tweaking a photo of a face. Ex: Making her smile, adding beard, changing eye color
+
+## Authors
+
+Cauim de Souza Lima
+Gabriel Fedrigo Barcik
+Victor Hugo Vianna
+
+##Instructions
+
+Open a shell and run
+```
+$ sudo ./face-tweak
+```
+
--- src/eye-detection/main.cpp
+++ src/eye-detection/main.cpp
@@ -1,153 +1,161 @@
-// The contents of this file are in the public domain. See LICENSE_FOR_EXAMPLE_PROGRAMS.txt
-/*
-
-    This example program shows how to find frontal human faces in an image and
-    estimate their pose.  The pose takes the form of 68 landmarks.  These are
-    points on the face such as the corners of the mouth, along the eyebrows, on
-    the eyes, and so forth.
-
-
-
-    The face detector we use is made using the classic Histogram of Oriented
-    Gradients (HOG) feature combined with a linear classifier, an image pyramid,
-    and sliding window detection scheme.  The pose estimator was created by
-    using dlib's implementation of the paper:
-       One Millisecond Face Alignment with an Ensemble of Regression Trees by
-       Vahid Kazemi and Josephine Sullivan, CVPR 2014
-    and was trained on the iBUG 300-W face landmark dataset (see
-    https://ibug.doc.ic.ac.uk/resources/facial-point-annotations/):
-       C. Sagonas, E. Antonakos, G, Tzimiropoulos, S. Zafeiriou, M. Pantic.
-       300 faces In-the-wild challenge: Database and results.
-       Image and Vision Computing (IMAVIS), Special Issue on Facial Landmark Localisation "In-The-Wild". 2016.
-    You can get the trained model file from:
-    http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2.
-    Note that the license for the iBUG 300-W dataset excludes commercial use.
-    So you should contact Imperial College London to find out if it's OK for
-    you to use this model file in a commercial product.
-
-
-    Also, note that you can train your own models using dlib's machine learning
-    tools.  See train_shape_predictor_ex.cpp to see an example.
-
-
-
-
-    Finally, note that the face detector is fastest when compiled with at least
-    SSE2 instructions enabled.  So if you are using a PC with an Intel or AMD
-    chip then you should enable at least SSE2 instructions.  If you are using
-    cmake to compile this program you can enable them by using one of the
-    following commands when you create the build project:
-        cmake path_to_dlib_root/examples -DUSE_SSE2_INSTRUCTIONS=ON
-        cmake path_to_dlib_root/examples -DUSE_SSE4_INSTRUCTIONS=ON
-        cmake path_to_dlib_root/examples -DUSE_AVX_INSTRUCTIONS=ON
-    This will set the appropriate compiler options for GCC, clang, Visual
-    Studio, or the Intel compiler.  If you are using another compiler then you
-    need to consult your compiler's manual to determine how to enable these
-    instructions.  Note that AVX is the fastest but requires a CPU from at least
-    2011.  SSE4 is the next fastest and is supported by most current machines.
-*/
-
-
-#include <dlib/image_processing/frontal_face_detector.h>
-#include <dlib/image_processing/render_face_detections.h>
-#include <dlib/image_processing.h>
-#include <dlib/gui_widgets.h>
-#include <dlib/image_io.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <stdlib.h>
 
-using namespace dlib;
+using namespace cv;
 using namespace std;
 
-// ----------------------------------------------------------------------------------------
 
-int main(int argc, char** argv)
+#define LEFT_EYE_BEGIN 37 // leftmost eye on the image
+#define LEFT_EYE_END 42
+#define RIGHT_EYE_BEGIN 43 //rightmost eye on the image
+#define RIGHT_EYE_END 48
+
+// Read points stored in the text files
+vector<Point2f> readPoints(string pointsFileName)
 {
-    try
+    vector<Point2f> points;
+    ifstream ifs(pointsFileName);
+    float x, y;
+    while(ifs >> x >> y)
     {
-        // This example takes in a shape model file and then a list of images to
-        // process.  We will take these filenames in as command line arguments.
-        // Dlib comes with example images in the examples/faces folder so give
-        // those as arguments to this program.
-        if (argc == 1)
-        {
-            cout << "Call this program like this:" << endl;
-            cout << "./face_landmark_detection_ex shape_predictor_68_face_landmarks.dat faces/*.jpg" << endl;
-            cout << "\nYou can get the shape_predictor_68_face_landmarks.dat file from:\n";
-            cout << "http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2" << endl;
-            return 0;
-        }
-
-        // We need a face detector.  We will use this to get bounding boxes for
-        // each face in an image.
-        frontal_face_detector detector = get_frontal_face_detector();
-        // And we also need a shape_predictor.  This is the tool that will predict face
-        // landmark positions given an image and face bounding box.  Here we are just
-        // loading the model from the shape_predictor_68_face_landmarks.dat file you gave
-        // as a command line argument.
-        shape_predictor sp;
-        deserialize(argv[1]) >> sp;
-
-
-        image_window win;
-        // Loop over all the images provided on the command line.
-        for (int i = 2; i < argc; ++i)
-        {
-            cout << "processing image " << argv[i] << endl;
-            array2d<rgb_pixel> img;
-            load_image(img, argv[i]);
-            // Make the image larger so we can detect small faces.
-            pyramid_up(img);
-
-            // Now tell the face detector to give us a list of bounding boxes
-            // around all the faces in the image.
-            std::vector<rectangle> dets = detector(img);
-            cout << "Number of faces detected: " << dets.size() << endl;
-
-            if(dets.size()>1)
-                cout<<"More than 1 face detected, errors may occur";
-
-
-            // Now we will go ask the shape_predictor to tell us the pose of
-            // each face we detected.
-            std::vector<full_object_detection> shapes;
-            for (unsigned long j = 0; j < dets.size(); ++j)
-            {
-                full_object_detection shape = sp(img, dets[j]);
-                cout << "number of parts: "<< shape.num_parts() << endl;
-
-                cout << "pixel position of first part:  " << shape.part(0) << endl;
-                cout << "pixel position of second part: " << shape.part(1) << endl;
-
-
-
-
-                // You get the idea, you can get all the face part locations if
-                // you want them.  Here we just store them in shapes so we can
-                // put them on the screen.
-                shapes.push_back(shape);
-            }
-
-            // Now let's view our face poses on the screen.
-            win.clear_overlay();
-            win.set_image(img);
-            //win.add_overlay(render_face_detections(shapes)); // prints the face detection
-
-            // We can also extract copies of each face that are cropped, rotated upright,
-            // and scaled to a standard size as shown here:
-            //dlib::array<array2d<rgb_pixel> > face_chips;
-            //extract_image_chips(img, get_face_chip_details(shapes), face_chips);
-            //win_faces.set_image(tile_images(face_chips));
-
-            cout << "Hit enter to process the next image..." << endl;
-            cin.get();
-        }
+        points.push_back(Point2f(x,y));
     }
-    catch (exception& e)
-    {
-        cout << "\nexception thrown!" << endl;
-        cout << e.what() << endl;
-    }
+
+    return points;
 }
 
-// ----------------------------------------------------------------------------------------
+// Apply affine transform calculated using srcTri and dstTri to src
+void applyAffineTransform(Mat &warpImage, Mat &src, vector<Point2f> &srcTri, vector<Point2f> &dstTri)
+{
 
+    // Given a pair of triangles, find the affine transform.
+    Mat warpMat = getAffineTransform( srcTri, dstTri );
+
+    // Apply the Affine Transform just found to the src image
+    warpAffine( src, warpImage, warpMat, warpImage.size(), INTER_LINEAR, BORDER_REFLECT_101);
+}
+
+// Warps and alpha blends triangular regions from img1 and img2 to img
+void morphTriangle(Mat &img1, Mat &img2, Mat &img, vector<Point2f> &t1, vector<Point2f> &t2, vector<Point2f> &t, double alpha)
+{
+
+    // Find bounding rectangle for each triangle
+    Rect r = boundingRect(t);
+    Rect r1 = boundingRect(t1);
+    Rect r2 = boundingRect(t2);
+
+    // Offset points by left top corner of the respective rectangles
+    vector<Point2f> t1Rect, t2Rect, tRect;
+    vector<Point> tRectInt;
+    for(int i = 0; i < 3; i++)
+    {
+        tRect.push_back( Point2f( t[i].x - r.x, t[i].y -  r.y) );
+        tRectInt.push_back( Point(t[i].x - r.x, t[i].y - r.y) ); // for fillConvexPoly
+
+        t1Rect.push_back( Point2f( t1[i].x - r1.x, t1[i].y -  r1.y) );
+        t2Rect.push_back( Point2f( t2[i].x - r2.x, t2[i].y - r2.y) );
+    }
+
+    // Get mask by filling triangle
+    Mat mask = Mat::zeros(r.height, r.width, CV_32FC3);
+    fillConvexPoly(mask, tRectInt, Scalar(1.0, 1.0, 1.0), 16, 0);
+
+    // Apply warpImage to small rectangular patches
+    Mat img1Rect, img2Rect;
+    img1(r1).copyTo(img1Rect);
+    img2(r2).copyTo(img2Rect);
+
+    Mat warpImage1 = Mat::zeros(r.height, r.width, img1Rect.type());
+    Mat warpImage2 = Mat::zeros(r.height, r.width, img2Rect.type());
+
+    applyAffineTransform(warpImage1, img1Rect, t1Rect, tRect);
+    applyAffineTransform(warpImage2, img2Rect, t2Rect, tRect);
+
+    // Alpha blend rectangular patches
+    Mat imgRect = (1.0 - alpha) * warpImage1 + alpha * warpImage2;
+
+    // Copy triangular region of the rectangular patch to the output image
+    multiply(imgRect,mask, imgRect);
+    multiply(img(r), Scalar(1.0,1.0,1.0) - mask, img(r));
+    img(r) = img(r) + imgRect;
+
+
+}
+
+int main( int argc, char** argv)
+{
+
+    string filename1("../hillary_clinton.jpg");
+    string filename2("../ted_cruz.jpg");
+
+    //alpha controls the degree of morph
+    double alpha = 0.5;
+
+    //Read input images
+    Mat img1 = imread(filename1);
+    Mat img2 = imread(filename2);
+
+    //convert Mat to float data type
+    img1.convertTo(img1, CV_32F);
+    img2.convertTo(img2, CV_32F);
+
+
+    //empty average image
+    Mat imgMorph = Mat::zeros(img1.size(), CV_32FC3);
+
+
+    //Read points
+    vector<Point2f> points1 = readPoints( filename1 + ".txt");
+    vector<Point2f> points2 = readPoints( filename2 + ".txt");
+    vector<Point2f> points;
+
+    //compute weighted average point coordinates
+    for(int i = 0; i < points1.size(); i++)
+    {
+        float x, y;
+        x = (1 - alpha) * points1[i].x + alpha * points2[i].x;
+        y =  ( 1 - alpha ) * points1[i].y + alpha * points2[i].y;
+
+        points.push_back(Point2f(x,y));
+
+    }
+
+
+    //Read triangle indices
+    ifstream ifs("tri.txt");
+    int x,y,z;
+
+    while(ifs >> x >> y >> z)
+    {
+        // Triangles
+        vector<Point2f> t1, t2, t;
+
+        // Triangle corners for image 1.
+        t1.push_back( points1[x] );
+        t1.push_back( points1[y] );
+        t1.push_back( points1[z] );
+
+        // Triangle corners for image 2.
+        t2.push_back( points2[x] );
+        t2.push_back( points2[y] );
+        t2.push_back( points2[z] );
+
+        // Triangle corners for morphed image.
+        t.push_back( points[x] );
+        t.push_back( points[y] );
+        t.push_back( points[z] );
+
+        morphTriangle(img1, img2, imgMorph, t1, t2, t, alpha);
+
+    }
+
+    // Display Result
+    imshow("Morphed Face", imgMorph / 255.0);
+    waitKey(0);
+
+    return 0;
+}
